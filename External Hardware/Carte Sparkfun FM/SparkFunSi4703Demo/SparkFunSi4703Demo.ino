19b959abdba1fcc1b723c37d65933935beb66beb
David de Lorenzo <de.lorenzo.david@gmail.com>
2020-04-16 23:01:15 +0200

    Adjust Si4703 demo

--- External Hardware/Carte Sparkfun FM/SparkFunSi4703Demo/SparkFunSi4703Demo.ino
+++ External Hardware/Carte Sparkfun FM/SparkFunSi4703Demo/SparkFunSi4703Demo.ino
@@ -1,5 +1,4 @@
 /* *******************************************************************************
- 1-6-2011
  Spark Fun Electronics 2011
  Nathan Seidle
  
@@ -12,7 +11,7 @@
       A5 : SCLK
       A4 : SDIO
       D2 : RST
-      A0 : Trimpot (optional)
+      A0 : Trimpot tuning (optional)
  
  Look for serial output at 57600 bps.
  
@@ -21,24 +20,23 @@
  1/18 - after much hacking, I suggest NEVER write to a register without first reading the contents of a chip.
  ie, don't updateRegisters without first readRegisters.
  
- If anyone manages to get this datasheet downloaded
- http://wenku.baidu.com/view/d6f0e6ee5ef7ba0d4a733b61.html
- Please let us know. It seem to be the latest version of the programming guide. It had a change on page 12 (write 0x8100 to 0x07)
- that allowed me to get the chip working..
+ If anyone manages to get this datasheet downloaded http://wenku.baidu.com/view/d6f0e6ee5ef7ba0d4a733b61.html
+ Please let us know. It seem to be the latest version of the programming guide. 
+ It had a change on page 12 (write 0x8100 to 0x07) that allowed me to get the chip working..
  
  Also, if you happen to find "AN243: Using RDS/RBDS with the Si4701/03", please share. I love it when companies refer to documents that don't exist.
  
  1/20 - Picking up FM stations from a plane flying over Portugal! Sweet! 93.9MHz sounds a little soft for my tastes,s but it's in Porteguese.
  
- ToDo:
- Display current status (from 0x0A) - done 1/20/11
- Add RDS decoding - works, sort of
- Volume Up/Down - done 1/20/11
- Mute toggle - done 1/20/11
- Tune Up/Down - done 1/20/11
- Read current channel (0xB0) - done 1/20/11
- Setup for Europe - done 1/20/11
- Seek up/down - done 1/25/11
+ Functionnalities:
+   - Display current status (from 0x0A)
+   - Add RDS decoding - works, sort of
+   - Volume Up/Down
+   - Mute toggle
+   - Tune Up/Down
+   - Read current channel (0xB0)
+   - Setup for Europe
+   - Seek up/down
  
  The Si4703 breakout does work with line out into a stereo or other amplifier. 
  Be sure to test with different length 3.5mm cables. Too short of a cable may degrade reception.
@@ -158,27 +156,28 @@ void loop()
     while (!Serial.available());
     option = Serial.read();
 
-    if(option == '1')  {
-      Serial.println("Tune to 97.3");
-      currentChannel = 973;
-      //currentChannel = 1023;
+    if(option == '1')  
+    {
+      Serial.println("Tune to 93.5");
+      currentChannel = 935;
       gotoChannel(currentChannel);
     }
-    else if(option == '2') {
+    else if(option == '2') 
+    {
       Serial.println("Mute toggle");
       si4703_readRegisters();
       si4703_registers[POWERCFG] ^= (1<<DMUTE); //Toggle Mute bit
       si4703_updateRegisters();
     }
-    else if(option == '3') {
+    else if(option == '3') 
+    {
       si4703_readRegisters();
-
       Serial.println();
       Serial.println("Radio Status:");
 
-      if(si4703_registers[STATUSRSSI] & (1<<RDSR)){
+      if(si4703_registers[STATUSRSSI] & (1<<RDSR))
+      {
         Serial.print(" (RDS Available)");
-
         byte blockerrors = (si4703_registers[STATUSRSSI] & 0x0600) >> 9; //Mask in BLERA
         if(blockerrors == 0) Serial.print (" (No RDS errors)");
         if(blockerrors == 1) Serial.print (" (1-2 RDS errors)");
@@ -195,7 +194,6 @@ void loop()
         Serial.print(" (Seek Successful!)");
       if(si4703_registers[STATUSRSSI] & (1<<AFCRL)) Serial.print(" (AFC/Invalid Channel)");
       if(si4703_registers[STATUSRSSI] & (1<<RDSS)) Serial.print(" (RDS Synch)");
-
       if(si4703_registers[STATUSRSSI] & (1<<STEREO)) 
         Serial.print(" (Stereo!)");
       else
@@ -206,34 +204,33 @@ void loop()
       Serial.print(rssi, DEC);
       Serial.println(" of 75)");
     }
-    else if(option == '4') {
+    else if(option == '4') 
+    {
       seek(SEEK_UP);
     }
-    else if(option == '5') {
+    else if(option == '5') 
+    {
       seek(SEEK_DOWN);
     }
-    else if(option == '6') {
+    else if(option == '6') 
+    {
       Serial.println("Poll RDS - x to exit");
-      while(1) {
-        if(Serial.available() > 0)
-          if(Serial.read() == 'x') break;
-
+      while(1) 
+      {
+        if(Serial.available()>0) if(Serial.read() == 'x') break;
         si4703_readRegisters();
-        if(si4703_registers[STATUSRSSI] & (1<<RDSR)){
+        if(si4703_registers[STATUSRSSI] & (1<<RDSR))
+        {
           Serial.println("We have RDS!");
           byte Ah, Al, Bh, Bl, Ch, Cl, Dh, Dl;
           Ah = (si4703_registers[RDSA] & 0xFF00) >> 8;
           Al = (si4703_registers[RDSA] & 0x00FF);
-
           Bh = (si4703_registers[RDSB] & 0xFF00) >> 8;
           Bl = (si4703_registers[RDSB] & 0x00FF);
-
           Ch = (si4703_registers[RDSC] & 0xFF00) >> 8;
           Cl = (si4703_registers[RDSC] & 0x00FF);
-
           Dh = (si4703_registers[RDSD] & 0xFF00) >> 8;
           Dl = (si4703_registers[RDSD] & 0x00FF);
-
           Serial.print("RDS: ");
           Serial.print(Ah);
           Serial.print(Al);
@@ -244,41 +241,44 @@ void loop()
           Serial.print(Dh);
           Serial.print(Dl);
           Serial.println(" !");
-
           delay(40); //Wait for the RDS bit to clear
         }
-        else {
+        else 
+        {
           Serial.println("No RDS");
           delay(30); //From AN230, using the polling method 40ms should be sufficient amount of time between checks
         }
         //delay(500);
       }
     }
-    else if(option == '8') {
+    else if(option == '8') 
+    {
       Serial.println("GPIO1 High");
       si4703_registers[SYSCONFIG1] |= (1<<1) | (1<<0);
       si4703_updateRegisters();
     }
-    else if(option == '9') {
+    else if(option == '9') 
+    {
       Serial.println("GPIO1 Low");
       si4703_registers[SYSCONFIG1] &= ~(1<<0);
       si4703_updateRegisters();
     }
-    else if(option == 'r') {
+    else if(option == 'r') 
+    {
       Serial.println("Print registers");
       si4703_printRegisters();
     }
-    else if(option == 't') {
+    else if(option == 't') 
+    {
       Serial.println("Trim pot tuning");
-      while(1) {
+      while(1) 
+      {
         if(Serial.available())
           if(Serial.read() == 'x') break;
-
         int trimpot = 0;
         for(int x = 0 ; x < 16 ; x++)
           trimpot += analogRead(A0);
         trimpot /= 16; //Take average of trimpot reading
-
         Serial.print("Trim: ");
         Serial.print(trimpot);
         trimpot = map(trimpot, 0, 1023, 875, 1079); //Convert the trimpot value to a valid station
@@ -288,33 +288,35 @@ void loop()
           currentChannel = trimpot;
           gotoChannel(currentChannel);
         }
-
         delay(100);
       }
     }
-    else if(option == 'v') {
-
+    else if(option == 'v') 
+    {
       byte current_vol;
-
       Serial.println();
       Serial.println("Volume:");
       Serial.println("+) Up");
       Serial.println("-) Down");
       Serial.println("x) Exit");
-      while(1){
-        if(Serial.available()){
+      while(1)
+      {
+        if(Serial.available())
+        {
           option = Serial.read();
-          if(option == '+') {
+          if(option == '+') 
+          {
             si4703_readRegisters(); //Read the current register set
             current_vol = si4703_registers[SYSCONFIG2] & 0x000F; //Read the current volume level
-            if(current_vol < 16) current_vol++; //Limit max volume to 0x000F
-            si4703_registers[SYSCONFIG2] &= 0xFFF0; //Clear volume bits
-            si4703_registers[SYSCONFIG2] |= current_vol; //Set new volume
-            si4703_updateRegisters(); //Update
+            if(current_vol < 15) current_vol++;          // Limit max volume to 0x000F
+            si4703_registers[SYSCONFIG2] &= 0xFFF0;      // Clear volume bits
+            si4703_registers[SYSCONFIG2] |= current_vol; // Set new volume
+            si4703_updateRegisters();                    // Update
             Serial.print("Volume: ");
             Serial.println(current_vol, DEC);
           }
-          if(option == '-') {
+          if(option == '-') 
+          {
             si4703_readRegisters(); //Read the current register set
             current_vol = si4703_registers[SYSCONFIG2] & 0x000F; //Read the current volume level
             if(current_vol > 0) current_vol--; //You can't go lower than zero
@@ -329,7 +331,8 @@ void loop()
       }
     }
 
-    else if(option == 'w') {
+    else if(option == 'w') 
+    {
       currentChannel = readChannel();
 #ifdef IN_EUROPE
       currentChannel += 1; //Increase channel by 100kHz
@@ -338,7 +341,8 @@ void loop()
 #endif
       gotoChannel(currentChannel);
     }
-    else if(option == 's') {
+    else if(option == 's') 
+    {
       currentChannel = readChannel();
 #ifdef IN_EUROPE
       currentChannel -= 1; //Decreage channel by 100kHz
@@ -347,7 +351,8 @@ void loop()
 #endif
       gotoChannel(currentChannel);
     }
-    else {
+    else 
+    {
       Serial.print("Choice = ");
       Serial.println(option);
     }
@@ -370,7 +375,7 @@ void gotoChannel(int newChannel)
   newChannel -= 8750; //9730 - 8750 = 980
 
 #ifdef IN_EUROPE
-    newChannel /= 10; //980 / 10 = 98
+  newChannel /= 10; //980 / 10 = 98
 #else
   newChannel /= 20; //980 / 20 = 49
 #endif
@@ -412,7 +417,8 @@ void gotoChannel(int newChannel)
  * Reads the current channel from READCHAN
  * Returns a number like 973 for 97.3MHz
  ******************************************************************************* */
-int readChannel(void) {
+int readChannel(void) 
+{
   si4703_readRegisters();
   int channel = si4703_registers[READCHAN] & 0x03FF; //Mask out everything but the lower 10 bits
 
@@ -435,7 +441,8 @@ int readChannel(void) {
  * Returns the freq if it made it
  * Returns zero if failed
  ******************************************************************************* */
-byte seek(byte seekDirection){
+byte seek(byte seekDirection)
+{
   si4703_readRegisters();
 
   // Set seek mode wrap bit
@@ -454,7 +461,6 @@ byte seek(byte seekDirection){
   {
     si4703_readRegisters();
     if((si4703_registers[STATUSRSSI] & (1<<STC)) != 0) break; //Tuning complete!
-
     Serial.print("Trying station:");
     Serial.println(readChannel());
   }
@@ -472,8 +478,9 @@ byte seek(byte seekDirection){
     Serial.println("Waiting...");
   }
 
+  //The bit was set indicating we hit a band limit or failed to find a station
   if(valueSFBL) 
-  { //The bit was set indicating we hit a band limit or failed to find a station
+  {
     Serial.println("Seek limit hit"); //Hit limit of band during seek
     return(FAIL);
   }
@@ -513,7 +520,7 @@ void si4703_init(void)
   si4703_registers[SYSCONFIG1] |= (1<<RDS); //Enable RDS
 
 #ifdef IN_EUROPE
-    si4703_registers[SYSCONFIG1] |= (1<<DE); //50kHz Europe setup
+  si4703_registers[SYSCONFIG1] |= (1<<DE); //50kHz Europe setup
   si4703_registers[SYSCONFIG2] |= (1<<SPACE0); //100kHz channel spacing for Europe
 #else
   si4703_registers[SYSCONFIG2] &= ~(1<<SPACE1 | 1<<SPACE0) ; //Force 200kHz channel spacing for USA
@@ -532,7 +539,7 @@ void si4703_init(void)
  * It's a little weird, you don't write an I2C addres
  * The Si4703 assumes you are writing to 0x02 first, then increments
  ******************************************************************************* */
- byte si4703_updateRegisters(void) 
+byte si4703_updateRegisters(void) 
 {
   Serial.println("updating registers");
 
@@ -540,7 +547,8 @@ void si4703_init(void)
   //A write command automatically begins with register 0x02 so no need to send a write-to address
   //First we send the 0x02 to 0x07 control registers
   //In general, we should not write to registers 0x08 and 0x09
-  for(int regSpot = 0x02 ; regSpot < 0x08 ; regSpot++) {
+  for(int regSpot = 0x02 ; regSpot < 0x08 ; regSpot++) 
+  {
     byte high_byte = si4703_registers[regSpot] >> 8;
     byte low_byte = si4703_registers[regSpot] & 0x00FF;
 
@@ -551,9 +559,10 @@ void si4703_init(void)
   Serial.println(" done");
   //End this transmission
   byte ack = Wire.endTransmission();
-  if(ack != 0) { //We have a problem! 
-    Serial.print("Write Fail:"); //No ACK!
-    Serial.println(ack, DEC); //I2C error: 0 = success, 1 = data too long, 2 = rx NACK on address, 3 = rx NACK on data, 4 = other error
+  if(ack != 0) 
+  { //We have a problem! 
+    Serial.print("Write Fail:"); // NACK!
+    Serial.println(ack, DEC);    // I2C error: 0 = success, 1 = data too long, 2 = rx NACK on address, 3 = rx NACK on data, 4 = other error
     return(FAIL);
   }
 
@@ -574,7 +583,8 @@ void si4703_readRegisters(void)
   //We may want some time-out error here
 
   //Remember, register 0x0A comes in first so we have to shuffle the array around a bit
-  for(int x = 0x0A ; ; x++) { //Read in these 32 bytes
+  for(int x = 0x0A ; ; x++) 
+  { //Read in these 32 bytes
     if(x == 0x10) x = 0; //Loop back to zero
     si4703_registers[x] = Wire.read() << 8;
     si4703_registers[x] |= Wire.read();
